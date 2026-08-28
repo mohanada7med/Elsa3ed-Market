51063b02603beb888cfed3d8d4d8679267f3fe2a
Mohanad Ahmed <ahmdmohanad28@gmail.com>
2026-08-28 14:39:51 +0300

    feat: migrate project from Vite to Next.js framework

--- api/index.ts
+++
@@ -1,8 +0,0 @@
-import type { Request, Response } from 'express';
-import { createApp } from '../server/app';
-
-const app = createApp();
-
-export default function handler(req: Request, res: Response) {
-  return app(req, res);
-}
